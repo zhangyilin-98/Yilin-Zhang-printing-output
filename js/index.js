52fa5d05bbdd6e568dc6d22bccd52a9930bcbef5
zhangyilin-98 <77629960+zhangyilin-98@users.noreply.github.com>
2021-01-25 20:40:09 +0800

    Added JS file

---
+++ js/index.js
@@ -0,0 +1,55 @@
+ let a = 3;
+ let b = 5;
+ let c;
+
+
+
+ let first_name = 'Yilin';
+ let last_name = 'Zhang';
+ let email = 'zhan0588@algonquinlive.com';
+
+
+ 
+ let output = '';
+ output += 'let a = 3;';
+ output += '\n';
+ output += 'let b = 5;';
+ output += '\n';
+ output += 'let c;';
+ output += '\n';
+ output += '------------------';
+ output += '\n';
+ output += 'a + b = ' + (a + b);
+ output += '\n';
+ output += 'a - b = ' + (a - b);
+ output += '\n';
+ output += 'a * b = ' + (a * b);
+ output += '\n';
+ output += 'a / b = ' + (a / b);
+ output += '\n';
+ output += 'a % b = ' + (a % b);
+ output += '\n';
+ output += '(a += b) : ' + (a += b);
+ output += '\n';
+ output += '(a -= b) : ' + (a -= b);
+ output += '\n';
+ output += '(a *= b) : ' + (a *= b);
+ output += '\n';
+ output += '(a /= b) : ' + (a /= b);
+ output += '\n';
+ output += '(a %= b) : ' + (a %= b);
+ output += '\n';
+ output += '(a == b) : ' + (a == b);
+ output += '\n';
+ output += '(a != b) : ' + (a != b);
+ output += '\n';
+ output += '(a > b) : ' + (a > b);
+ output += '\n';
+ output += '(a < b) : ' + (a < b);
+ output += '\n';
+ output += '(!a && !c) : ' + (!a && !c);
+ output += '\n';
+ output += '(!a || !c) : ' + (!a || !c);
+ output += '\n';
+ output += 'My name is' + ' ' + first_name + ' ' + last_name + '.' + ' ' + 'You can contact me at' + ' ' + email + '.';
+alert(output);
